06f8ed0f7af756b1671007131920674c92834e59
JUNG-MIN WOO <jayxwoo@gmail.com>
2020-10-18 08:02:53 +1300

    init

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// ========== imports ==========
+import './default.js';
+
+// ========== DOM references ==========
+
+
+// ========== global variables ==========
+
+
+// ========== script ==========
