eb85c409ba9eca806092643140567ab6d2d38577
JUNG-MIN WOO <jayxwoo@gmail.com>
2020-10-26 10:13:17 +1300

    working on adding chats to database

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 import './default.js';
 
 // ========== DOM references ==========
-
+const chatForm = document.querySelector('.chat-form');
 
 // ========== global variables ==========
 const col = 'chats';
@@ -24,11 +24,29 @@ class ChatGetter {
     }
 }
 
+// add chats
+class ChatAdder {
+    constructor(col) {
+        this.col = col;
+    }
+}
+
 
 // main
 const main = function () {
     // get chats
     const chatGetter = new ChatGetter(col);
     chatGetter.get();
+
+    // add chats
+    chatForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        // get input value
+        const chatInput = chatForm.chat.value.trim();
+        console.log(chatInput);
+
+        chatForm.reset();
+    });
 };
 main();
