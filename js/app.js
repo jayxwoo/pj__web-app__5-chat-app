39640e068e7f8bf7c74a928fbeed941264e6b8d1
JUNG-MIN WOO <jayxwoo@gmail.com>
2020-10-26 08:08:07 +1300

    connect database / get database data

--- js/app.js
+++ js/app.js
@@ -5,6 +5,36 @@ import './default.js';
 
 
 // ========== global variables ==========
-
+const col = 'chats';
 
 // ========== script ==========
+// get chats
+class ChatGetter {
+    constructor(col) {
+        this.col = col;
+    }
+
+    get = async function () {
+        // get collection
+        const col = await firebase.firestore().collection(this.col).get();
+        console.log(col);
+
+        // get document
+        const docs = col.docs;
+        console.log(docs);
+
+        // get properties
+        docs.forEach(doc => { 
+            console.log(doc.data());
+        });
+    }
+}
+
+
+// main
+const main = function () {
+    // get chats
+    const chatGetter = new ChatGetter(col);
+    chatGetter.get();
+};
+main();
